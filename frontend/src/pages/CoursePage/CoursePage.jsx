5dbba74eb346953fdc7deef7def122b57252298c
Mit Kapadia <mit_rupang@yahoo.ca>
2020-12-26 18:51:54 -0500

    Prettier version of JSON object displayed now

--- frontend/src/pages/CoursePage/CoursePage.jsx
+++ frontend/src/pages/CoursePage/CoursePage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./CoursePage.module.css";
 import { Navigation } from "../../components/Navigation/Navigation";
 import { TreeViewer } from "../../components/TreeViewer/TreeViewer";
@@ -6,13 +6,37 @@ import { useParams } from "react-router-dom";
 import { courseData } from "../../model/courseData";
 
 export const CoursePage = () => {
-  let { courseId } = useParams();
+  const { courseId } = useParams();
+
+  const [error, setError] = useState(null);
+  const [treeObj, setTreeObj] = useState({});
+
+  useEffect(() => {
+    fetch(`http://localhost:3003/get-tree-by-id/${courseId}`)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setTreeObj(result);
+        },
+
+        (error) => {
+          setError(error);
+        }
+      );
+  }, [courseId]);
+
+  let Viewer = null;
+  if (error) {
+    Viewer = <h2>{`Error: ${error.message}`}</h2>;
+  } else {
+    Viewer = <TreeViewer data={treeObj} />;
+  }
 
   return (
     <>
       <Navigation />
       <h1 className={styles.heading}>{courseData[courseId].name}</h1>
-      <TreeViewer courseId={courseId} />
+      {Viewer}
     </>
   );
 };
