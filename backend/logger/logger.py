da646051ef4af9137dd3a082f2ef12930ee368e6
rafiiary <arya.rafii@mail.utoronto.ca>
2021-01-14 15:44:35 -0500

    Fixed bug with wrong line number. Added functionality for both the warn and error as well

--- backend/logger/logger.py
+++ backend/logger/logger.py
@@ -13,24 +13,28 @@ class Logger:
 		self.file_name = file_name
 		self.fileHandler = FileHandler(self.file_name)
 		self.writer = self.fileHandler.getWriteHandle()
-		self.frameinfo = getframeinfo(stack()[1][0])
 	
 	def debug(self, message):
 		''' Write a debug message to the file using the writer
 		'''
 		if self.level > 0:
 			return
-		self.writer.write("DEBUG: " + self.frameinfo.filename +\
-						":" + str(self.frameinfo.lineno + 1) + " : " + message + "\n")
+		frameinfo = getframeinfo(stack()[1][0])
+		self.writer.write("DEBUG: " + frameinfo.filename +\
+						":" + str(frameinfo.lineno) + " : " + message + "\n")
 
-	def warning(self, message):
+	def warn(self, message):
 		'''Write a warning message to the file using the writer
 		'''
 		if self.level > 1:
 			return
-		self.writer.write("WARNING: " + message + "\n")
+		frameinfo = getframeinfo(stack()[1][0])
+		self.writer.write("WARNING: " + frameinfo.filename +\
+						":" + str(frameinfo.lineno) + " : " + message + "\n")
 
 	def error(self, message):
 		'''Write an error message to the file using the writer
 		'''
-		self.writer.write("ERROR: " + message + "\n")
+		frameinfo = getframeinfo(stack()[1][0])
+		self.writer.write("ERROR: " + frameinfo.filename +\
+						":" + str(frameinfo.lineno) + " : " + message + "\n")
