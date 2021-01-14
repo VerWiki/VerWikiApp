b384481e533b90871e518adfd1f1e3b9db0cda3c
rafiiary <arya.rafii@mail.utoronto.ca>
2021-01-14 11:34:37 -0500

    Added error and warning capability. Level can now determine what messages to show

--- backend/logger/logger.py
+++ backend/logger/logger.py
@@ -15,9 +15,25 @@ class Logger:
 	def debug(self, message):
 		''' Write a debug message to the file using the writer
 		'''
+		if self.level > 0:
+			return
 		self.writer.write("DEBUG: " + message + "\n")
-	
+
+	def warning(self, message):
+		'''Write a warning message to the file using the writer
+		'''
+		if self.level > 1:
+			return
+		self.writer.write("WARNING: " + message + "\n")
+
+	def error(self, message):
+		'''Write an error message to the file using the writer
+		'''
+		self.writer.write("ERROR: " + message + "\n")
+
 if __name__ == "__main__":
-	logger = Logger("omg")
+	logger = Logger(level=2)
 	logger.debug("This is my first debug message")
+	logger.warning("This is my first warning message")
+	logger.error("This is my first error message")
 	logger.fileHandler.closeFile()
