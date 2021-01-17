078cd5276970057dbefedcfb8933823da46a7c55
slakh <s.lakhani@hotmail.ca>
2021-01-17 14:20:01 -0500

    Extract renderTree and animateTree functionality into a separate function

--- frontend/src/components/Tree/Tree.jsx
+++ frontend/src/components/Tree/Tree.jsx
@@ -4,6 +4,90 @@ import { select, hierarchy, tree, linkHorizontal } from "d3";
 import ResizeObserver from "resize-observer-polyfill";
 import "./Tree.module.css";
 
+function animateTree(nodeGroupEnter, enteringAndUpdatingLinks) {
+  nodeGroupEnter
+    .attr("opacity", 0)
+    .transition()
+    .duration(500)
+    .delay((node) => node.depth * 300)
+    .attr("opacity", 1);
+
+  enteringAndUpdatingLinks
+    .attr("stroke-dashoffset", function () {
+      return this.getTotalLength();
+    })
+    .transition()
+    .duration(500)
+    .delay((link) => link.source.depth * 500)
+    .attr("stroke-dashoffset", 0);
+}
+
+function renderTree(dimensions, jsonData, svgRef, onNodeClick) {
+  const svg = select(svgRef.current);
+  const { width, height } = dimensions;
+  const marginLeft = 70;
+  const marginTop = 30;
+
+  // Transform hierarchical data
+  const root = hierarchy(jsonData);
+  const treeLayout = tree().size([height, width]);
+
+  // Creates the links between nodes
+  const linkGenerator = linkHorizontal()
+    .x((link) => link.y + marginLeft)
+    .y((link) => link.x + marginTop);
+
+  // Enrich hierarchical data with coordinates
+  treeLayout(root);
+
+  // Create the node group, which will hold the nodes and labels
+  const nodeGroup = svg.selectAll(".node-group").data(root.descendants());
+  const nodeGroupEnter = nodeGroup.enter().append("g");
+
+  nodeGroupEnter
+    .merge(nodeGroup)
+    .attr("class", "node-group")
+    .attr(
+      "transform",
+      (node) => `translate(${node.y + marginLeft},${node.x + marginTop})`
+    )
+    .style("cursor", "pointer")
+    .on("click", onNodeClick);
+
+  nodeGroup.exit().remove();
+
+  // Add nodes to the node group
+  nodeGroupEnter
+    .append("circle")
+    .merge(nodeGroup.select("circle"))
+    .attr("r", 4);
+
+  // Add labels to the node group
+  nodeGroupEnter
+    .append("text")
+    .merge(nodeGroup.select("text"))
+    .attr("text-anchor", "middle")
+    .attr("font-size", 18)
+    .attr("y", -15)
+    .text((node) => node.data.name);
+
+  // Add links between nodes and animate them in
+  const enteringAndUpdatingLinks = svg
+    .selectAll(".link")
+    .data(root.links())
+    .join("path")
+    .attr("class", "link")
+    .attr("d", linkGenerator)
+    .attr("stroke-dasharray", function () {
+      const length = this.getTotalLength();
+      return `${length} ${length}`;
+    })
+    .attr("stroke", "black")
+    .attr("fill", "none")
+    .attr("opacity", 1);
+  return [nodeGroupEnter, enteringAndUpdatingLinks];
+}
+
 function usePrevious(value) {
   const ref = useRef();
   useEffect(() => {
@@ -41,87 +125,16 @@ export function Tree({ jsonData, onNodeClick }) {
 
   // Will be called initially and on every data change
   useEffect(() => {
-    const svg = select(svgRef.current);
-    const { width, height } = dimensions;
-    const marginLeft = 70;
-    const marginTop = 30;
-
-    // Transform hierarchical data
-    const root = hierarchy(jsonData);
-    const treeLayout = tree().size([height, width]);
-
-    // Creates the links between nodes
-    const linkGenerator = linkHorizontal()
-      .x((link) => link.y + marginLeft)
-      .y((link) => link.x + marginTop);
-
-    // Enrich hierarchical data with coordinates
-    treeLayout(root);
-
-    // Create the node group, which will hold the nodes and labels
-    const nodeGroup = svg.selectAll(".node-group").data(root.descendants());
-    const nodeGroupEnter = nodeGroup.enter().append("g");
-
-    nodeGroupEnter
-      .merge(nodeGroup)
-      .attr("class", "node-group")
-      .attr(
-        "transform",
-        (node) => `translate(${node.y + marginLeft},${node.x + marginTop})`
-      )
-      .style("cursor", "pointer")
-      .on("click", onNodeClick);
-
-    nodeGroup.exit().remove();
-
-    // Add nodes to the node group
-    nodeGroupEnter
-      .append("circle")
-      .merge(nodeGroup.select("circle"))
-      .attr("r", 4);
-
-    // Add labels to the node group
-    nodeGroupEnter
-      .append("text")
-      .merge(nodeGroup.select("text"))
-      .attr("text-anchor", "middle")
-      .attr("font-size", 18)
-      .attr("y", -15)
-      .text((node) => node.data.name);
-
-    // Add links between nodes and animate them in
-    const enteringAndUpdatingLinks = svg
-      .selectAll(".link")
-      .data(root.links())
-      .join("path")
-      .attr("class", "link")
-      .attr("d", linkGenerator)
-      .attr("stroke-dasharray", function () {
-        const length = this.getTotalLength();
-        return `${length} ${length}`;
-      })
-      .attr("stroke", "black")
-      .attr("fill", "none")
-      .attr("opacity", 1);
-
+    const [nodeGroupEnter, enteringAndUpdatingLinks] = renderTree(
+      dimensions,
+      jsonData,
+      svgRef,
+      onNodeClick
+    );
     // This is needed so the animations don't happen again every
     // time we resize the window
     if (jsonData !== previouslyRenderedData) {
-      nodeGroupEnter
-        .attr("opacity", 0)
-        .transition()
-        .duration(500)
-        .delay((node) => node.depth * 300)
-        .attr("opacity", 1);
-
-      enteringAndUpdatingLinks
-        .attr("stroke-dashoffset", function () {
-          return this.getTotalLength();
-        })
-        .transition()
-        .duration(500)
-        .delay((link) => link.source.depth * 500)
-        .attr("stroke-dashoffset", 0);
+      animateTree(nodeGroupEnter, enteringAndUpdatingLinks);
     }
   }, [jsonData, dimensions, previouslyRenderedData, onNodeClick]);
 
