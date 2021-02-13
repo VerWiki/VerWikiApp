a8b295a4a243d88048cdbd0f7840d3eb93b1e658
Mit Kapadia <mit_rupang@yahoo.ca>
2021-02-13 03:09:38 -0500

    Done button is now disabled if input is invalid

--- frontend/src/components/NodePathHistory/NodePathHistory.jsx
+++ frontend/src/components/NodePathHistory/NodePathHistory.jsx
@@ -5,8 +5,6 @@ import Button from "@material-ui/core/Button";
 import Breadcrumbs from "@material-ui/core/Breadcrumbs";
 import TextField from "@material-ui/core/TextField";
 import ClickAwayListener from "@material-ui/core/ClickAwayListener";
-import Snackbar from "@material-ui/core/Snackbar";
-import Alert from "@material-ui/lab/Alert";
 import NavigateNext from "@material-ui/icons/NavigateNext";
 import EditRounded from "@material-ui/icons/EditRounded";
 import DoneRounded from "@material-ui/icons/DoneRounded";
@@ -23,6 +21,11 @@ function convertPathToString(path) {
   return path.join("/") + "/";
 }
 
+function convertStringToPath(string) {
+  // Remove preceding or trailing slashes and spaces
+  return string.trim().replace(/^\/+/, "").replace(/\/+$/, "").split("/");
+}
+
 export const NodePathHistory = ({
   path,
   onNodeNameClick,
@@ -31,11 +34,25 @@ export const NodePathHistory = ({
 }) => {
   const [mode, setMode] = useState(MODES.VIEW);
   const [currentText, setCurrentText] = useState("");
-  const [errorOpen, setErrorOpen] = React.useState(false);
+  const [isCurrentTextValid, setIsCurrentTextValid] = useState(false);
 
+  /**
+   * Whenever the path is updated from the parent component,
+   * set the current text to the path it represents.
+   */
   useEffect(() => {
     setCurrentText(convertPathToString(path));
   }, [path]);
+
+  /**
+   * Any time that the current text is updated, which can happen
+   * when the parent changes the path prop, or if the user types
+   * in the text field, check if what is typed is valid or not
+   * and store it.
+   */
+  useEffect(() => {
+    setIsCurrentTextValid(isValidPath(convertStringToPath(currentText)));
+  }, [currentText, isValidPath]);
 
   /**
    * Store the current text being typed
@@ -45,27 +62,16 @@ export const NodePathHistory = ({
   };
 
   /**
-   * Triggered when the user is finished editing the path
+   * Triggered when the user is finished editing a VALID path
    * and clicks done. Notifies the parent of the change
    * through a callback, passing in the new path as a parameter.
    */
   const onDoneClickHandler = () => {
-    // Remove preceding or trailing slashes and spaces
-    const newPath = currentText
-      .trim()
-      .replace(/^\/+/, "")
-      .replace(/\/+$/, "")
-      .split("/");
+    const newPath = convertStringToPath(currentText);
 
-    // Inform the parent container of the change, if there was one and
-    // given that the path is valid
+    // Inform the parent container of the change, if there was one
     if (currentText !== convertPathToString(path)) {
-      if (isValidPath(newPath)) {
-        onPathChange(newPath);
-      } else {
-        setErrorOpen(true);
-        clearChanges();
-      }
+      onPathChange(newPath);
     }
   };
 
@@ -76,18 +82,6 @@ export const NodePathHistory = ({
   const clearChanges = () => {
     setCurrentText(convertPathToString(path));
     setMode(MODES.VIEW);
-  };
-
-  /**
-   * This function is called when the alert close button
-   * is clicked or there is a timeout.
-   */
-  const handleClose = (event, reason) => {
-    // Should not close if the user just clicks anywhere
-    if (reason === "clickaway") {
-      return;
-    }
-    setErrorOpen(false);
   };
 
   const Links = path.slice(0, -1).map((name, index) => (
@@ -133,9 +127,9 @@ export const NodePathHistory = ({
         size="small"
         onChange={onTextChangeHandler}
         onKeyDown={(e) => {
-          // If the enter key is pressed, it is equivalent
-          // to clicking on Done
-          if (e.keyCode === 13) onDoneClickHandler();
+          // If the enter key is pressed, and the text is valid
+          // it is equivalent to clicking on Done
+          if (e.keyCode === 13 && isCurrentTextValid) onDoneClickHandler();
         }}
       />
     </ClickAwayListener>
@@ -157,6 +151,7 @@ export const NodePathHistory = ({
       color="primary"
       variant="outlined"
       size="large"
+      disabled={!isCurrentTextValid}
       onClick={onDoneClickHandler}
     >
       <DoneRounded />
@@ -165,18 +160,6 @@ export const NodePathHistory = ({
 
   return (
     <Box display="flex" alignItems="center">
-      {/* Alert to display if the path entered is invalid */}
-      <Snackbar
-        open={errorOpen}
-        autoHideDuration={4000}
-        onClose={handleClose}
-        anchorOrigin={{ vertical: "top", horizontal: "left" }}
-      >
-        <Alert onClose={handleClose} severity="error">
-          The path is invalid!
-        </Alert>
-      </Snackbar>
-
       <Box>{mode === MODES.VIEW ? BreadcrumbsView : EditPathView}</Box>
       <Box pl={1.5}>{mode === MODES.VIEW ? EditButton : DoneButton}</Box>
     </Box>
