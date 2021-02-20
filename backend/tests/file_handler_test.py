055cd8ec530024346921684ba3e620331adc73f5
rafiiary <arya.rafii@mail.utoronto.ca>
2021-02-20 16:03:27 -0500

    File handler tests done

--- backend/tests/file_handler_test.py
+++ backend/tests/file_handler_test.py
@@ -8,6 +8,8 @@ import os
 sys.path.append('../logger')
 from file_handler import FileHandler
 
+NAME_OF_LOGS_DIR = "logs"
+
 class FileHandlerTest(unittest.TestCase):
     def setup(self):
         self.file_handler = FileHandler("test_name")
@@ -62,10 +64,10 @@ class FileHandlerTest(unittest.TestCase):
             with mock.patch('builtins.open', mock_open):
                 self.assertRaises(IOError, self.file_handler.open_new_file_write)
 
-    @patch('os.close')
+    @patch('file_handler.FileHandler.close_file')
     @patch('builtins.open')
     @patch('os.path.exists')
-    def test_open_new_file_write_existing_file(self, _dir_exists, mock_open, mock_close):
+    def test_open_new_file_write_existing_directory(self, _dir_exists, mock_open, mock_close):
         self.setup()
         _dir_exists.return_value = True
         mock_close.return_value = True
@@ -73,12 +75,60 @@ class FileHandlerTest(unittest.TestCase):
         self.file_handler.open_new_file_write()
         self.assertEqual("opened", self.file_handler.file_handle)
         
-#    def test_open_new_file_write_non_existing_file(self):
+    @patch('os.mkdir')
+    @patch('file_handler.FileHandler.close_file')
+    @patch('builtins.open')
+    @patch('os.path.exists')
+    def test_open_new_file_write_non_existing_directory(self, _dir_exists, mock_open, mock_close, _mkdir):
+        self.setup()
+        _dir_exists.return_value = False
+        mock_close.return_value = True
+        mock_open.return_value = "opened"
+        self.file_handler.open_new_file_write()
+        self.assertEqual("opened", self.file_handler.file_handle)
 
-#    def test_get_write_handle_returns_handle(self):
-#        self.setup()
-#        self.file_handler.open_new_file_write.return_value = mock_open(read_data='')
-#        assert(
+    @patch('file_handler.FileHandler.close_file')
+    @patch('builtins.open')
+    @patch('os.path.exists')
+    @patch('os.mkdir')
+    @patch('file_handler.FileHandler.open_new_file_write')
+    def test_get_write_handle_returns_handle(self, _open_new_file, _mkdir, _dir_exists, _open, _close):
+        self.setup()
+
+        _open_new_file.side_effect = self.set_file_handle_to_non_null
+        self.file_handler.get_write_handle()
+
+        self.assertTrue(self.file_handler.file_handle != None)
+
+        self.file_handler.get_write_handle()
+        _open_new_file.assert_called_once()
+
+
+    def test_get_full_path_is_string(self):
+        self.setup()
+        full_path = self.file_handler.get_full_path()
+        assert(isinstance(full_path, str))
+
+    def test_get_full_path_contains_name(self):
+        self.setup()
+        full_path = self.file_handler.get_full_path()
+        assert(self.file_handler.file_name in full_path)
+
+    def test_get_full_path_contains_dir_name(self):
+        self.setup()
+        full_path = self.file_handler.get_full_path()
+        assert(NAME_OF_LOGS_DIR in full_path)
+
+    @patch('file_handler.FileHandler.close_file')
+    def test_close_file_throws_exception(self, _close):
+        self.setup()
+        self.set_file_handle_to_non_null()
+        _close.side_effect = IOError()
+
+        self.assertRaises(IOError, self.file_handler.close_file)
+
+    def set_file_handle_to_non_null(self):
+        self.file_handler.file_handle = MagicMock()
 
 if __name__ == '__main__':
     unittest.main()
