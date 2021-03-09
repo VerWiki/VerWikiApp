e776ab0321336bb337ebaf3453ad805bbb7ff101
slakh <s.lakhani@hotmail.ca>
2021-03-09 08:41:42 -0500

    Inprogress work of trying to make onHover work

--- backend/server.py
+++ backend/server.py
@@ -96,10 +96,7 @@ def _get_content_from_site(url: str) -> str:
         # Prepend https://cwsl.ca to all links which were previously local links
         if len(anchor["href"]) > 0 and anchor["href"][0] == "/":
             anchor["href"] = f"https://cwsl.ca{anchor['href']}"
-        print("got here")
-        anchor["onmouseenter"] = "console.log(this)"
-        print("got here after mouseover")
-        print(anchor)
+        anchor["onMouseEnter"] = "{highlightRelatedNode}"
     print(content)
 
     return str(content)
--- frontend/src/components/InfoWindow/InfoWindow.jsx
+++ frontend/src/components/InfoWindow/InfoWindow.jsx
@@ -1,3 +1,6 @@
+import React from "react";
+import DOMPurify from "dompurify";
+
 /**
  * The react component for the information window for each node
  * in the concept tree.
@@ -5,14 +8,24 @@
  * sanitization is done to the incoming htmlInfo using DOMPurify
  */
 
-import React from "react";
-import DOMPurify from "dompurify";
-
 export const InfoWindow = ({ info }) => {
-  const safeHTML = DOMPurify.sanitize(info);
+  //const safeHTML = DOMPurify.sanitize(info);
+
+  const highlightRelatedNode = () => {
+    console.log("Reached here");
+  };
+
+  const safeHTML = info;
   return (
     <div>
       <h2>Wiki Information</h2>
+      <a
+        href="https://google.com"
+        onMouseEnter={highlightRelatedNode}
+        onMouseLeave={highlightRelatedNode}
+      >
+        Link
+      </a>
       <div
         id="infoWindowDiv"
         dangerouslySetInnerHTML={{ __html: safeHTML }}
