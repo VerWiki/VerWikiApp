ac6e5e781fb2409611b55608b80cd0a01be764e3
SL <s.lakhani1996@gmail.com>
2021-03-17 20:55:37 -0400

    Add comment and plan for how to do constants

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,4 +1,5 @@
-// Config values for the entire application.
+// Config values for the entire application. Ensure to document each constant
+//Within each config we have treeConfig, and then some other config; each subcategory is related to the component that uses it
 export const Config = Object.freeze({
   MAX_DEPTH: 2,
   FADE_OPACITY: 0.25,
