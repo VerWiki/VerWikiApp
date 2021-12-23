ce5d9edd8ca9254f61091ef5e4878712a13cc543
Mit Kapadia <mit_rupang@yahoo.ca>
2021-12-22 21:06:46 -0500

    Fix json parsing script

--- backend/parse_json.py
+++ backend/parse_json.py
@@ -1,9 +1,10 @@
 """
 Preconditions:
+- The first line contains the root node and its url
 - Each new level has more tabs/spaces than the level before it
 - Items on the same level have the same number of tabs/spaces
 - The name and url should be on the SAME line (if the url exists)
-- The url should start with https
+- The url should start with http
 """
 import sys
 import json
@@ -13,36 +14,53 @@ def create_new_node(name, url):
     return {"name": name, "url": url, "children": []}
 
 
+def parse_line(line):
+    https_index = line.find("http")
+    if https_index == -1:
+        name = line
+        url = ""
+    else:
+        name = line[:https_index].rstrip()
+        url = line[https_index:].rstrip()
+
+    return name, url
+
+
 def main(input_filename, output_filename):
-    json_obj = {"name": "<root_name>", "url": "<url>", "children": []}
+    json_obj = {"name": "", "url": "", "children": []}
     stack = [json_obj]
+    levels = []
 
     with open(input_filename, "r") as file:
+        # Root node
+        first_line = file.readline().strip()
+        name, url = parse_line(first_line)
+        json_obj["name"] = name
+        json_obj["url"] = url
+
         for line in file:
             line = line.rstrip()
             if line == "":
                 continue
 
+            # Allows for the usage of both tabs or spaces
             curr_level = len(line) - len(line.lstrip())
-            line = line.lstrip()
+            if not levels or levels[-1] < curr_level:
+                levels.append(curr_level)
+            curr_level_index = levels.index(curr_level)
 
-            https_index = line.find("https")
-            if https_index == -1:
-                name = line
-                url = ""
-            else:
-                name = line[:https_index].rstrip()
-                url = line[https_index:].rstrip()
+            line = line.lstrip()
+            name, url = parse_line(line)
 
             new_node = create_new_node(name, url)
 
-            stack[curr_level]["children"].append(new_node)
+            stack[curr_level_index]["children"].append(new_node)
 
             # Add another space on the stack if needed
-            if len(stack) == curr_level + 1:
+            if len(stack) == curr_level_index + 1:
                 stack.append(None)
 
-            stack[curr_level + 1] = new_node
+            stack[curr_level_index + 1] = new_node
 
     with open(output_filename, "w") as file:
         json.dump(json_obj, file, ensure_ascii=False, indent=4)
